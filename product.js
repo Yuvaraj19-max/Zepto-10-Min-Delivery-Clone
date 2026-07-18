026d34f92fcf590357a577f656f3fe508f8f0fc7
LODAGALA YUVARAJ <126891273+Yuvaraj19-max@users.noreply.github.com>
2026-07-18 13:28:14 +0530

    Zepto clone files

---
+++ product.js
@@ -0,0 +1,15 @@
+// %Fetch Address Functionlity
+let userLocation =  document.getElementById("location")
+userLocation.addEventListener("click",()=>{
+    navigator.geolocation.getCurrentPosition((position)=>{
+        let latitude = position.coords.latitude
+        let longitude = position.coords.longitude
+        let locationApi = `https://nominatim.openstreetmap.org/reverse?lat=${latitude}&lon=${longitude}&format=jsonv2`
+        let fetchingArea = async()=>{
+            let response = await fetch(locationApi)
+            let {address:{suburb,city}} = await response.json()
+            userLocation.innerHTML=`<i class="fa-solid fa-location-dot"></i> ${suburb},${city}`
+        }
+        fetchingArea()
+    })
+})
